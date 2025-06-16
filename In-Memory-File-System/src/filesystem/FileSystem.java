973bf507f0cdeef33e90df3fd33d85dd4929f722
Ansh2625 <anshsharma2625@gmail.com>
2025-06-16 18:24:27 +0530

    Added searchContent command using KMP algorithm

--- In-Memory-File-System/src/filesystem/FileSystem.java
+++ In-Memory-File-System/src/filesystem/FileSystem.java
@@ -407,4 +407,97 @@ public class FileSystem
         return getAbsolutePath(node.getParent()) + "/" + node.getName();
     }
 
+
+
+    // Search Command
+    public void searchContent(String pattern) 
+    {
+        Set<Node> visited = new HashSet<>();
+        dfsSearchContent(current, pattern, visited, getAbsolutePath(current));
+    }
+
+    // DFS search
+    private void dfsSearchContent(Node node, String pattern, Set<Node> visited, String pathSoFar) 
+    {
+        if (visited.contains(node)) return;
+        visited.add(node);
+
+        String fullPath = pathSoFar.equals("/") ? "/" + node.getName() : pathSoFar + "/" + node.getName();
+
+        if (node.isFile()) 
+        {
+            if (kmpSearch(node.getContent(), pattern))
+                System.out.println(fullPath);
+        } 
+        else 
+        {
+            for (Map.Entry<String, Node> entry : node.getChildren().entrySet())
+            {
+                Node child = entry.getValue();
+                Node target = (child.getSymbolicLink() != null) ? child.getSymbolicLink() : child;
+                dfsSearchContent(target, pattern, visited, fullPath);
+            }
+        }
+    }
+
+    // Kmp Search Algorithm
+    private boolean kmpSearch(String text, String pattern) 
+    {
+        if (pattern == null || pattern.isEmpty()) return true;
+        if (text == null || text.isEmpty()) return false;
+
+        int[] lps = buildLPS(pattern);
+        int i = 0, j = 0;
+
+        while (i < text.length())
+        {
+            if (text.charAt(i) == pattern.charAt(j))
+            {
+                i++;
+                j++;
+                if (j == pattern.length())
+                    return true;
+            } 
+            else 
+            {
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i++;
+            }
+        }
+
+        return false;
+    }
+
+    // helper to build LPS 
+    private int[] buildLPS(String pattern) 
+    {
+        int[] lps = new int[pattern.length()];
+        int len = 0;
+        int i = 1;
+
+        while (i < pattern.length())
+        {
+            if (pattern.charAt(i) == pattern.charAt(len))
+            {
+                len++;
+                lps[i] = len;
+                i++;
+            } 
+            else 
+            {
+                if (len != 0) 
+                {
+                    len = lps[len - 1];
+                } else 
+                {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+
 }
