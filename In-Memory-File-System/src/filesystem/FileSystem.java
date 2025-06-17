969913dbf6d18f6a13085dd9f043156d889f4f90
Ansh2625 <anshsharma2625@gmail.com>
2025-06-17 11:08:57 +0530

    Added Undo redo logic in mkdir command

--- In-Memory-File-System/src/filesystem/FileSystem.java
+++ In-Memory-File-System/src/filesystem/FileSystem.java
@@ -43,6 +43,8 @@ public class FileSystem
         // Break path into folder names
         String[] parts = path.split("/");
 
+        Stack<String> created = new Stack<>(); // track what we create
+
         for(String part : parts)
         {
             if(part.isEmpty()) // skip "", due to leading /
@@ -52,12 +54,20 @@ public class FileSystem
             if(!temp.hasChild(part))
             {
                 Node folder = new Node(part, false); // it is a Folder
-                insertToTrie(part);
+                folder.setParent(temp);
                 temp.addChild(part, folder);
+                insertToTrie(part);
+                created.push(part); // track for undo
             }
 
             // Move in to the next folder
             temp = temp.getChild(part);
+        }
+
+        if (!created.isEmpty())
+        {
+            undoStack.push(new Action(Action.ActionType.MKDIR, path));
+            redoStack.clear();
         }
     }
 
